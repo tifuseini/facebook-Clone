cbbbe2b9372d13a1721ba2155590abde90b7af5c
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-03 15:11:55 +0000

    TOp header

--- facebook-web/components/Header.js
+++ facebook-web/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header = () =>{
-    return <div>Header</div>
+    return (
+        <div className="sticky top-0 z-50 flex items-center h-16 p-2 bg-white shadow-md">
+            <h1 className="text-3xl font-bold underline ">
+                Hello world!
+            </h1>
+        </div>
+      )
 };
 
 export default Header;
