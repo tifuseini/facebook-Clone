2a4957bc901c12ae2d488db58d072de223b3a50c
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-03 15:22:21 +0000

    Image Domain

--- facebook-web/components/Header.js
+++ facebook-web/components/Header.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 const Header = () =>{
     return (
         <div className="sticky top-0 z-50 flex items-center h-16 p-2 bg-white shadow-md">
-            <h1 className="text-3xl font-bold underline ">
-                Hello world!
-            </h1>
+
+            <div className="flex min-w-fit">
+                <Image
+                src="https://upload.wikimedia.org/wikipedia/en/0/04/Facebook_f_logo_%282021%29.svg"
+                height={40}
+                width={40}
+                 />
+            </div>
+            
+            
         </div>
       )
 };
